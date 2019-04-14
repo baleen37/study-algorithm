9e59560058aa28a45547eb6a5c1c6254d2f1cb24
Jiho Lee <baleen37@gmail.com>
2019-04-14 14:31:58 +0900

    Add 11718, 11719

--- baekjoon/11719.cpp
+++ baekjoon/11719.cpp
@@ -4,10 +4,8 @@ using namespace std;
 
 int main(){
   string str;
-  while (true) {
+  for (int i=0; i<100; i++) {
     getline(cin, str);
-    if (str=="")
-      break;
     cout << str << endl;
   }
 
